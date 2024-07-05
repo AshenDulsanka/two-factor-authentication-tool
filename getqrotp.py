a6dcb70487a78a1cfcc8699d7659fee76b468f66
Ashen Abeysekara <ashendul@gmail.com>
2024-07-05 11:44:46 +0530

    2fa working

--- getqrotp.py
+++ getqrotp.py
@@ -2,12 +2,5 @@ import time
 import pyotp
 import qrcode
 
-key = pyotp.random_base32()
+key = "testqrkey"
 
-name = input("Enter your name: ")
-
-uri = pyotp.totp.TOTP(key).provisioning_uri(name=name, issuer_name="2FA Test")
-
-print(uri)
-
-qrcode.make(uri).save("otp.png")
--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import time
 import pyotp
+import qrcode
 
-key = pyotp.random_base32()
+key = "JBSWY3DPEHPK3PXP"
+
+name = input("Enter your name: ")
+
+uri = pyotp.totp.TOTP(key).provisioning_uri(name=name, issuer_name="2FA Test")
+
+print(uri)
+
+qrcode.make(uri).save("otp.png")
 
 totp = pyotp.TOTP(key)
 
-print(totp.now())
-
-input_code = input("Enter 2FA Code: ")
-
-print(totp.verify(input_code))
+while True:
+    print(totp.verify(input("Enter OTP: ")))
