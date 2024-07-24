6df7791a2a780178086242a00a1d9d59b5931c4a
Ashen Abeysekara <ashendul@gmail.com>
2024-07-25 00:46:50 +0530

    functionalities done, need to make some ui changes

--- main.py
+++ main.py
@@ -17,30 +17,44 @@ totp = pyotp.TOTP(key)
 def gen_qr():
     name = name_box.get()
     uri = pyotp.totp.TOTP(key).provisioning_uri(name=name, issuer_name="2FA Test")
-    qrcode.make(uri).save("otp.png")
-    print(uri)
-    img = PhotoImage(file="otp.png")
-    img_label = Label(window, image=img)
-    img_label.pack(pady=20)
+    qr = qrcode.make(uri)
+    qr.save("otp.png")
+    img = Image.open("otp.png")
+    img = ImageTk.PhotoImage(img)
+    img_label.config(image=img)
+    img_label.image = img
+    otp_frame.pack(pady=10)
 
-    lf = LabelFrame(window, text="Scan the QR Code and enter the OTP")
-    lf.pack(pady=10)
-
-    otp_box = Entry(lf, font=("Poppins, 24"), width=25, justify=CENTER)
-    otp_box.pack(pady=10, padx=10)
+def verify_otp():
+    otp = otp_box.get()
+    if totp.verify(otp):
+        result_label.config(text="OTP Valid!", fg="green")
+    else:
+        result_label.config(text="OTP Invalid!", fg="red")
 
 lf = LabelFrame(window, text="Enter your name or email")
 lf.pack(pady=10)
 
-name_box = Entry(lf, font=("Poppins, 24"), width=25, justify=CENTER)
+name_box = Entry(lf, font=("Poppins", 24), width=25, justify=CENTER)
 name_box.pack(pady=10, padx=10)
-
-name = name_box.get()
 
 button_frame = Frame(window)
 button_frame.pack(pady=20)
 
 gen_button = Button(button_frame, text="Generate QR Code", command=gen_qr)
-gen_button.grid(row=0, column=0, padx=10)
+gen_button.pack()
+
+img_label = Label(window)
+img_label.pack(pady=20)
+
+otp_frame = LabelFrame(window, text="Scan the QR Code and enter the OTP")
+otp_box = Entry(otp_frame, font=("Poppins", 24), width=25, justify=CENTER)
+otp_box.pack(pady=10, padx=10)
+
+verify_button = Button(otp_frame, text="Verify OTP", command=verify_otp)
+verify_button.pack(pady=10)
+
+result_label = Label(otp_frame, font=("Poppins", 18))
+result_label.pack(pady=10)
 
 window.mainloop()
